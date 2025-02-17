c38b5bf6d39e926d2fce8c86159e2bfc0c85e633
roxtooth <benjamin.d.carlson@gmail.com>
2025-02-17 12:14:42 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export', // Ensures Fleek can deploy a static site
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // Fixes Next.js image issues with IPFS
+  },
+};
+
+module.exports = nextConfig;
 
