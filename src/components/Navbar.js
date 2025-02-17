64223bf23948062804a6b7129809fad008970c18
roxtooth <benjamin.d.carlson@gmail.com>
2025-02-17 22:43:45 +0000

    Fixed WalletConnect auto-popup & invalid app configuration

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,25 @@
-import { useEffect, useState } from 'react';
-import { connectWallet } from '../utils/wallet';
+import { useState } from "react";
+import { connectWallet } from "../utils/wallet";
 
 export default function Navbar() {
-  const [walletAddress, setWalletAddress] = useState('');
+  const [walletAddress, setWalletAddress] = useState("");
 
-  useEffect(() => {
-    async function fetchWallet() {
-      const walletData = await connectWallet();
-      if (walletData && walletData.address) {
-        setWalletAddress(walletData.address);
-      } else {
-        console.error("No wallet address found.");
-      }
+  const handleConnectWallet = async () => {
+    const walletData = await connectWallet();
+    if (walletData && walletData.address) {
+      setWalletAddress(walletData.address);
+    } else {
+      console.error("No wallet address found.");
     }
-    fetchWallet();
-  }, []);
+  };
 
   return (
     <nav className="nav">
       <h1>Scrooge $DuckVault Presale</h1>
-      <button onClick={connectWallet}>
-        {walletAddress ? walletAddress.substring(0, 6) + '...' + walletAddress.slice(-4) : 'Connect Wallet'}
+      <button onClick={handleConnectWallet}>
+        {walletAddress
+          ? walletAddress.substring(0, 6) + "..." + walletAddress.slice(-4)
+          : "Connect Wallet"}
       </button>
     </nav>
   );
