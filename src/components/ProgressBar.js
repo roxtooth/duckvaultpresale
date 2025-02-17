4a70a653815997d7b3d4aa109cc1e0a842477dcc
roxtooth <benjamin.d.carlson@gmail.com>
2025-02-17 10:48:29 -0600

    Create ProgressBar.js

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,9 @@
+export default function ProgressBar({ progress }) {
+  return (
+    <div className="progress-container">
+      <div className="progress-bar" style={{ width: `${progress}%` }}>
+        {progress}%
+      </div>
+    </div>
+  );
+}
