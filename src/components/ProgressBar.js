516b96f490a57c8e5da1643300e06ac32dc5b454
roxtooth <benjamin.d.carlson@gmail.com>
2025-02-19 10:27:27 -0600

    Delete src/components directory

--- src/components/ProgressBar.js
+++
@@ -1,9 +0,0 @@
-export default function ProgressBar({ progress }) {
-  return (
-    <div className="progress-container">
-      <div className="progress-bar" style={{ width: `${progress}%` }}>
-        {progress}%
-      </div>
-    </div>
-  );
-}
