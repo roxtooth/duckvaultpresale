e0a054aeedfb7ddceeb668d4654702900f5fe1bd
roxtooth <benjamin.d.carlson@gmail.com>
2025-02-19 22:36:27 +0000

    Update changes again

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { config } from '../lib/wagmi'; // Import the wagmi config
+
+// Create a client for react-query
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
     <WagmiProvider config={config}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </WagmiProvider>
   );
 }
